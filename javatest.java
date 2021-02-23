ad42cb182b01c8a0625249d58296e461c1e65524
Christian Loos <christianloos43@gmail.com>
2021-02-23 13:15:49 +0100

    second commit

--- javatest.java
+++ javatest.java
@@ -1,3 +1,10 @@
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Map;
 
 class javatest{  
     public static void main(String args[]){  
